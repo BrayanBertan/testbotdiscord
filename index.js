a3803448219a2db3999432381c122e84968a1526
brayanbertan <brayanbertan@gmail.com>
2021-04-02 19:22:02 -0300

    create get set

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ client.on('ready', () => {
 
 
 client.on('message', msg => {
+
   async  function createMain(nick){
    await prisma.user.create({
     data:{
@@ -29,25 +30,65 @@ client.on('message', msg => {
   });
   }
 
-  async function getAllAlts(){
-    const allUsers = await prisma.user.findMany({
+  async function getAlts(nick){
+    const allAlts = await prisma.user.findFirst({
+      where:{
+          nick:nick,
+      },
       include: {
       alts:true
       },
     });
-   return allUsers;
+   return allAlts;
   }
+
+  async  function setAlt(mainId,nick){
+    await prisma.alt.create({
+     data:{
+       nick:nick,
+       mainId:mainId
+     }
+   });
+   }
 
   var contentArray = msg.content.trim().split(' ');
   var command = contentArray[0];
   if(contentArray.length == 1) return msg.reply('Invalid Command! missing params!');
+  var param1 = contentArray[1].toUpperCase();
 
   if (command === 'create') {
-    createMain(contentArray[1].toUpperCase());
-    msg.reply('Main '+ contentArray[1]+' created!');
+    createMain(param1).then(function(value){
+      msg.reply('Main '+ param1+' created!');
+    },
+    function(error){
+      msg.reply('Error');
+    });
   }
   if (command === 'get') {
-    msg.reply(getAllAlts());
+    getAlts(param1).then(function(value) {
+     if(value == null) return msg.reply(param1+' doesnt exists!');
+     var result = value.alts.map((value, index) => value['nick'] );
+     msg.reply(param1+' alts: '+JSON.stringify(result));
+    }, function(error) {
+      msg.reply('Error');
+    });
+  }
+
+  if (command === 'set') {
+    if(contentArray.length < 3) return msg.reply('Invalid Command! missing params!');
+    var param2 = contentArray[2].toUpperCase();
+    getAlts(param1).then(function(value) {
+      if(value == null) return msg.reply(param1+' doesnt exists!');
+      setAlt(value.id,param2).then(function(value){
+        msg.reply('Alt '+param2+ ' created for Main '+ param1);
+      },
+      function(error){
+        msg.reply('Error');
+      });
+     
+     }, function(error) {
+      msg.reply('Error');
+     });
   }
 });
 
