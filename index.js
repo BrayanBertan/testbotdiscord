1fb9dfae84d7c71903d31af7fdc8a94669042b9b
brayanbertan <brayanbertan@gmail.com>
2021-04-02 20:15:06 -0300

    check if main already exists

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ client.on('message', msg => {
   });
   }
 
+
+  async function deleteAlt(nick){
+    const deleteUser = await prisma.alt.delete({
+      where: {
+        nick: 'nick',
+      },
+    })
+  }
+
+  async function getMainId(nick){
+    const mainId = await prisma.user.findFirst({
+      where:{
+          nick:nick,
+      },
+    });
+   return mainId;
+  }
+
   async function getAlts(nick){
     const allAlts = await prisma.user.findFirst({
       where:{
@@ -46,7 +64,7 @@ client.on('message', msg => {
     await prisma.alt.create({
      data:{
        nick:nick,
-       mainId:mainId
+       mainId:mainId.id
      }
    });
    }
@@ -64,12 +82,21 @@ client.on('message', msg => {
   var param1 = contentArray[1].toUpperCase();
 
   if (command === 'create') {
-    createMain(param1).then(function(value){
-      msg.reply('Main '+ param1+' created!');
+    getMainId(param1).then(function(main){
+      if(main != null) return msg.reply('Main already registered!');
+      createMain(param1).then(function(value){
+        msg.reply('Main '+ param1+' created!');
+      },
+      function(error){
+        msg.reply('Error');
+      });
     },
     function(error){
       msg.reply('Error');
     });
+
+
+  
   }
   if (command === 'get') {
     getAlts(param1).then(function(value) {
@@ -81,7 +108,7 @@ client.on('message', msg => {
     });
   }
 
-  if (command === 'set') {
+  if (command === 'teest') {
     if(contentArray.length < 3) return msg.reply('Invalid Command! missing params!');
     var param2 = contentArray[2].toUpperCase();
     getAlts(param1).then(function(value) {
@@ -107,6 +134,37 @@ client.on('message', msg => {
      }, function(error) {
       msg.reply('Error');
      });
+  }
+
+  
+  if(command === 'set'){
+    if(contentArray.length < 3) return msg.reply('Invalid Command! missing params!');
+      var param2 = contentArray[2].toUpperCase();
+    getMainId(param1).then(function(main){
+      if(main == null) return msg.reply(param1+' doesnt exists!');
+      var altsArray = param2.trim().split(',');
+      if(altsArray.length == 1){
+        setAlt(main,param2).then(function(value){
+          msg.reply('Alt '+param2+ ' created for Main '+ param1);
+        },
+        function(error){
+          msg.reply('Error setAlt');
+        });
+      }else{
+        var allAlts = [];
+        altsArray.forEach(item => allAlts.push({nick:item, mainId:main.id}));
+        setManyAlts(allAlts).then(function(value){
+          msg.reply('Alts '+altsArray+ ' created for Main '+ param1);
+        },
+        function(error){
+          msg.reply('Error setManyAlts');
+        });
+      }
+      
+    },
+    function(error){
+      msg.reply('Error getMainId');
+    });
   }
 });
 
